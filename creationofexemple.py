b4e0e7dde300bbc7003fd1d60e138c10f27cc820
MeloKiki <kiliangaillard25@gmail.com>
2025-05-08 16:53:47 +0200

    Génération aléatoire de graphe

--- creationofexemple.py
+++ creationofexemple.py
@@ -1,29 +1,30 @@
+import random
 import task_pb2
 
-# Crée une instance de TaskGraph
-task_graph = task_pb2.TaskGraph()
+def generate_random_dag(n, p, wcet_range=(1, 10)):
+    task_graph = task_pb2.TaskGraph()
 
-# Ajoute quelques tâches manuellement
-t1 = task_graph.tasks.add()
-t1.id = 1
-t1.wcet = 3
-t1.dependencies.extend([])
+    for i in range(n):
+        task = task_graph.tasks.add()
+        task.id = i
+        task.wcet = random.randint(*wcet_range)
 
-t2 = task_graph.tasks.add()
-t2.id = 2
-t2.wcet = 2
-t2.dependencies.extend([1])
+        # Ajoute des dépendances vers des tâches plus anciennes (i.e. j < i)
+        for j in range(i):
+            if random.random() < p:
+                task.dependencies.append(j)
 
-t3 = task_graph.tasks.add()
-t3.id = 3
-t3.wcet = 4
-t3.dependencies.extend([1])
+    return task_graph
 
-t4 = task_graph.tasks.add()
-t4.id = 4
-t4.wcet = 2
-t4.dependencies.extend([2, 3])
+def save_task_graph(task_graph, filename="exemple.bin"):
+    with open("exemple.bin", "wb") as f:
+        f.write(task_graph.SerializeToString())
 
-# Sauvegarde dans le fichier exemple.bin
-with open("exemple.bin", "wb") as f:
-    f.write(task_graph.SerializeToString())
+if __name__ == "__main__":
+    n = 10        # nombre de tâches
+    p = 0.3       # probabilité d'un arc
+    wcet_range = (1, 10)
+
+    graph = generate_random_dag(n, p, wcet_range)
+    save_task_graph(graph)
+    print(f"{n} tâches générées avec p = {p} et sauvegardées dans exemple.bin")
